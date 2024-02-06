5d094d35cc3534f3e5f9e034280c96c9960e7236
grizimin <grizimin@gmaill.com>
2024-02-06 16:55:43 +0500

    Added Справка in the menu

--- view/window.py
+++ view/window.py
@@ -1,13 +1,17 @@
 from tkinter import *
+#from view.menu_ref import *
 
+
+# Инициалиализация окна
 window = Tk()
 window.title("CarVision")
 window.geometry("1200x700")
-window.title("Geeeks For Geeks")
+window.title("CarVision")
 window.resizable(False, False)
 window.option_add("*tearOff", FALSE)
+#/
 
-
+# Объявление меню
 main_menu = Menu()
 file_menu = Menu()
 file_menu.add_cascade(label="Импорт")
@@ -15,7 +19,12 @@ file_menu.add_cascade(label="Экспорт")
 file_menu.add_separator()
 file_menu.add_cascade(label="Выйти")
 
-main_menu.add_cascade(label="Файл", menu=file_menu)
+info_menu = Menu()
+info_menu.add_cascade(label="GitHub")
+info_menu.add_cascade(label="Документация")
 
+main_menu.add_cascade(label="Файл", menu=file_menu)
+main_menu.add_cascade(label="Справка", menu=info_menu)
 
 window.config(menu=main_menu)
+#/
