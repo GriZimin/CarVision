afa916e9571b345a7b2b036cd83a220b972e2ad4
grizimin <grizimin@gmaill.com>
2024-02-09 18:51:01 +0500

    Small Refactor

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-from view.window import window
-from view.menu_defs import *
+import tkinter as tk
+import view.view_defs as defs
+from tkinter import messagebox
+
+def edit_click():
+    messagebox.showinfo("GUI Python", "Нажата опция Edit")
+
+window = tk.Tk()
+window.geometry("1200x700")
+window.title("CarVision")
+window.resizable(False, False)
+window.option_add("*tearOff", False)
+
+# Объявление меню
+main_menu = tk.Menu()
+file_menu = tk.Menu()
+file_menu.add_cascade(label="Импорт", command=edit_click)
+file_menu.add_cascade(label="Экспорт")
+file_menu.add_separator()
+file_menu.add_cascade(label="Выйти", command=exit)
+
+info_menu = tk.Menu()
+info_menu.add_cascade(label="GitHub")
+info_menu.add_cascade(label="Документация")
+
+main_menu.add_cascade(label="Файл", menu=file_menu)
+main_menu.add_cascade(label="Справка", menu=info_menu)
+
+window.config(menu=main_menu)
 
 window.mainloop()
