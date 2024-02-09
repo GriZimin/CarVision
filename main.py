7d9e06408a9d9d005a0782473f2161b4ac15cb7c
grizimin <grizimin@gmaill.com>
2024-02-09 20:29:03 +0500

    Made Выйти, GitHub and Документация buttons work

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import tkinter as tk
 import view.view_defs as defs
 from tkinter import messagebox
 
-def edit_click():
-    messagebox.showinfo("GUI Python", "Нажата опция Edit")
-
 window = tk.Tk()
 window.geometry("1200x700")
 window.title("CarVision")
@@ -12,16 +9,16 @@ window.resizable(False, False)
 window.option_add("*tearOff", False)
 
 # Объявление меню
-main_menu = tk.Menu()
+main_menu = tk.Menu(window)
 file_menu = tk.Menu()
-file_menu.add_cascade(label="Импорт", command=edit_click)
-file_menu.add_cascade(label="Экспорт")
+file_menu.add_command(label="Импорт", command=defs.check)
+file_menu.add_command(label="Экспорт")
 file_menu.add_separator()
-file_menu.add_cascade(label="Выйти", command=exit)
+file_menu.add_command(label="Выйти", command=exit)
 
 info_menu = tk.Menu()
-info_menu.add_cascade(label="GitHub")
-info_menu.add_cascade(label="Документация")
+info_menu.add_command(label="GitHub", command=defs.openGitHub)
+info_menu.add_command(label="Документация", command=defs.openDocumentation)
 
 main_menu.add_cascade(label="Файл", menu=file_menu)
 main_menu.add_cascade(label="Справка", menu=info_menu)
--- view/view_defs.py
+++ view/view_defs.py
@@ -1,5 +1,9 @@
-def exit():
-    exit()
-
+import webbrowser
 def check():
     print("Hello World")
+
+def openGitHub():
+    webbrowser.open("https://github.com/GriZimin/ComputerVision")
+
+def openDocumentation():
+    webbrowser.open("grizimin.github.io/ComputerVision/")
