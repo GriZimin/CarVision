f1335adf8104b6dcb556b26f91507899c46ecfae
EG24 <egor.galkin09@gmail.com>
2024-02-13 02:16:58 +0500

    pip install ultralytics pip install imageai pip install yolo5 pip install torch pip install tensorflow and i am sad

--- test1.py
+++ test1.py
@@ -1,3 +1,11 @@
 import cv2
 import numpy as np
+import tensorflow as tf
+from imageai.Detection import ObjectDetection
 
+detector = ObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath("yolov8n.pt")
+detector.loadModel(1)
+
+detector.detectObjectsFromImage(input_image="123.jpg",output_type="new_image.jpg")
