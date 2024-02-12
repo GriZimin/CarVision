aa9ad07561a64e025ac30138a0d6800614ddf529
EG24 <egor.galkin09@gmail.com>
2024-02-13 02:29:08 +0500

    pip install pytorch update README

--- test1.py
+++ test1.py
@@ -5,7 +5,7 @@ from imageai.Detection import ObjectDetection
 
 detector = ObjectDetection()
 detector.setModelTypeAsYOLOv3()
-detector.setModelPath("yolov8n.pt")
+detector.setModelPath("yolo-tiny.h5")
 detector.loadModel(1)
 
 detector.detectObjectsFromImage(input_image="123.jpg",output_type="new_image.jpg")
