a9f24a36aacb5e669b7af01633fd2ba426990295
EG24 <egor.galkin09@gmail.com>
2024-02-15 12:41:39 +0500

    created directories interface update

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ ctk.set_default_color_theme("blue")
 
 # Инициализаия окна
 window = ctk.CTk()
-window.geometry("960x506")
+window.geometry("960x540")
 window.title("CarVision")
 window.resizable(False, False)
 window.option_add("*tearOff", False)
+window.wm_iconbitmap()
+icopath = ImageTk.PhotoImage(file="images/favicon.png")
+window.iconphoto(True, icopath)
 window.tk.call('wm', 'iconphoto', window, tk.PhotoImage(file="images/favicon.png"))
 #/
 
 # Инициализация меню
 menu_frame = ctk.CTkFrame(master=window)
-menu_frame.grid(row=0, pady=10, padx=0, sticky="w")
+menu_frame.grid(row=0, pady=10, padx=10, sticky="w")
 
 file_menu = ctk.CTkOptionMenu(menu_frame, values=["Импорт", "Экспорт", "Выйти"], command=defs.FileMenuHandler)
 file_menu.grid(row=0, column=0, pady=10, padx=10)
@@ -32,9 +35,9 @@ help_menu.set("Справка")
 
 # Инициализация картинки
 image_frame = ctk.CTkFrame(master=window)
-image_frame.grid(row=1, pady=10, padx=0)
+image_frame.grid(row=1, pady=10, padx=10)
 
-image = Image.open('II_image/finishImage.jpg').resize((711, 400))
+image = Image.open('II_image/finishImage.jpg').resize((711, 430))
 image_tk = ImageTk.PhotoImage(image)
 label = ttk.Label(image_frame, image=image_tk)
 label.grid(row=2, column=0, padx=10, pady=10, sticky="nsw")
@@ -43,8 +46,8 @@ label.grid(row=2, column=0, padx=10, pady=10, sticky="nsw")
 # Инициализация текстового поля
 text_box = ctk.CTkTextbox(master=window)
 text_box.insert("0.0", "Distance: 0 \n\nObject: 0")
-text_box.configure(width=200, height=400)
-text_box.grid(row=1, column=1, padx=10, pady=10)
+text_box.configure(state="disabled", width=200, height=455, text_color="DarkCyan", border_width=5, font=("1", 15))
+text_box.grid(row=1, column=1, padx=0, pady=10)
 
 window.mainloop()
 
