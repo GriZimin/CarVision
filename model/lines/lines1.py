dcab77a1f9c5ee8d4ce1ec67056614d70010a209
EG24 <egor.galkin09@gmail.com>
2024-03-05 19:27:20 +0500

    lines

--- model/lines/lines1.py
+++ model/lines/lines1.py
@@ -3,26 +3,16 @@ import cv2
 import model.main
 import detect
 
-video = cv2.VideoCapture("win-20221219-16-35-22-pro_MvA292jB.mp4")
+def middle_line_picture(picture):
 
-if not video.isOpened():
-    print("0")
-
-cv2.waitKey(10)
-
-a=0
-
-while video.isOpened():
-
-    pon, frame = video.read()
-    image = frame
+    image = picture
     copy_img = np.copy(image)
 
-    cv2.namedWindow("Video", cv2.WINDOW_AUTOSIZE)
-    cv2.resizeWindow('Video', 960, 540)
+    cv2.namedWindow("picture", cv2.WINDOW_AUTOSIZE)
+    cv2.resizeWindow('picture', 960, 540)
 
-    image = model.main.detect(frame)
-    color_coverted = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # image = model.main.detect(picture)
+    # color_coverted = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     width = 960
     height = 540
@@ -45,11 +35,61 @@ while video.isOpened():
 
     cr_line = cv2.line(final_frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 0), 2)
 
-    cv2.imshow('Video', final_frame)
+    # cv2.imshow('picture', final_frame)
+    #
+    # cv2.waitKey(0)
 
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        video.release()
-        cv2.destroyAllWindows()
+    return x1, y1, x2, y2
 
-video.release()
-cv2.destroyAllWindows()
+def middle_line_video(video):
+
+    if not video.isOpened():
+        print("0")
+
+    cv2.waitKey(10)
+
+    a=0
+
+    while video.isOpened():
+
+        pon, frame = video.read()
+        image = frame
+        copy_img = np.copy(image)
+
+        cv2.namedWindow("Video", cv2.WINDOW_AUTOSIZE)
+        cv2.resizeWindow('Video', 960, 540)
+
+        # image = model.main.detect(frame)
+        # color_coverted = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        width = 960
+        height = 540
+        dim = (width, height)
+        frameF = cv2.resize(image, dim)
+        copy_img = cv2.resize(copy_img, dim)
+
+        copy_img = detect.mask(copy_img)
+        copy_img = detect.cenny(copy_img)
+
+        lines = cv2.HoughLinesP(copy_img, 2, np.pi / 180, 100, np.array([()]), 40, 2)
+        a_lines = detect.average_slope_intercept(copy_img, lines)
+        line_img = detect.display_lines(frameF, a_lines)
+        final_frame = cv2.addWeighted(frameF, 0.8, line_img, 0.5, 1)
+
+        x1 = (a_lines[0][0] + a_lines[1][0]) / 2
+        x2 = (a_lines[0][2] + a_lines[1][2]) / 2
+        y1 = (a_lines[0][1] + a_lines[1][1]) / 2
+        y2 = (a_lines[0][3] + a_lines[1][3]) / 2
+
+        cr_line = cv2.line(final_frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 0), 2)
+
+        cv2.imshow('Video', final_frame)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            video.release()
+            cv2.destroyAllWindows()
+
+    video.release()
+    cv2.destroyAllWindows()
+
+print(middle_line_picture(cv2.imread("WIN_20221219_16_40_07_Pro.jpg")))
